608778196780a43b8bec95832bf8f43383243c3b
Akis <b_kalogeropoulos@yahoo.gr>
2022-11-16 18:26:59 +0200

    Initialize config babel, tsconfig, settings, package.json, metro.config

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,30 @@
 /**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+* Metro configuration for React Native, with Svg support.
+*
+* @see https://facebook.github.io/metro/docs/configuration
+*/
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+const {
+resolver: { assetExts, sourceExts },
+} = await getDefaultConfig();
+
+return {
+resetCache: true,
+resolver: {
+assetExts: assetExts.filter(ext => ext !== 'svg'),
+sourceExts: [...sourceExts, 'svg'],
+},
+transformer: {
+babelTransformerPath: require.resolve('react-native-svg-transformer'),
+getTransformOptions: async () => ({
+transform: {
+experimentalImportSupport: false,
+inlineRequires: true,
+},
+}),
+},
 };
+})();
